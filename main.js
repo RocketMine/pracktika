729a8e044887eabd06ab433de2268da203b4fa8a
Roman Peskov <romanpskv@gmail.com>
2022-11-25 00:45:42 +0300

    Сайт

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = function str(){
+    for(var i=0;i<4; i++){
+            for (var y=0;y<4;y++){
+                document.getElementById('game').innerHTML+="<div class='block'></div>";
+            }
+    }	
+    }
--- main2.js
+++ main2.js
@@ -1,16 +1,17 @@
-function drawTable(){                                       //Имя функции
-    var element = document.createElement ("table"); 
-    element.setAttribute("border", "1");                    //задаем атрибуты
-    element.setAttribute("id", "tab");
-    element.addEventListener("click", testr, false);
-    for (var i=0; i<2; i++){
-        var row=element.insertRow(i);
-        for(var j=0; j<5; j++){
-            var cell = row.insertCell(j);
-            cell.width = "100";
-            cell.height = "100";
-            cell ["id"]="".concat(i,j);
-            cell.appendChild(document.createTextNode((i+1)+(j+1)));
-        }
-    }
+
+    function init(){
+        sec = 0;
+        setInterval(tick, 1000);
+}
+function tick(){
+        sec++;
+        document.getElementById("timer").
+            childNodes[0].nodeValue = sec;
+}
+window.onload = function str(){
+    for(var i=0;i<4; i++){
+        for (var y=0;y<4;y++){
+             document.getElementById('game').innerHTML+="<div class='block'></div>";
+}
+}	
 }
