0f0c37f05ac6f4cbbf3923e77724f0bbb3bd6088
Roman Peskov <romanpskv@gmail.com>
2022-11-28 04:01:40 +0300

    Функции и т.д

--- main2.js
+++ main2.js
@@ -1,21 +1,105 @@
+alert("Здравствуйте, вы должны пройти некий тест на время");
 
-function init(){
-        sec = 0;
-        setInterval(tick, 1000);
+function otvet1(){
+    let pr1 = document.getElementById("pr1").value
+        pr1 = Number.parseInt(pr1)
+        if(pr1 == 90){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
 }
-function tick(){
-        sec++;
-        document.getElementById("timer").
-            childNodes[0].nodeValue = sec;
+function otvet2(){
+    let pr2 = document.getElementById("pr2").value
+        pr2 = Number.parseInt(pr2)
+        if(pr2 == 48){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
 }
-function getRandomInRange(min, max){
-    return Math.floor(Math.random()*(max-min+1))
+function otvet3(){
+    let pr3 = document.getElementById("pr3").value
+        pr3 = Number.parseInt(pr3)
+        if(pr3 == 9){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
 }
-console.log(getRandomInRange(1, 30))
-
-function prim(){
-    var a = getRandomInRange(0, 10);
-    var b = getRandomInRange(10, 20);
-    var c = a + b ;   
+function otvet4(){
+    let pr4 = document.getElementById("pr4").value
+        pr4 = Number.parseInt(pr4)
+        if(pr4 == 118){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
 }
-console.log(c)
+function otvet5(){
+    let pr5 = document.getElementById("pr5").value
+        pr5 = Number.parseInt(pr5)
+        if(pr5 == 1){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
+}
+function otvet6(){
+    let pr6 = document.getElementById("pr6").value
+        pr6 = Number.parseInt(pr6)
+        if(pr6 == 60){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
+}
+function otvet7(){
+    let pr7 = document.getElementById("pr7").value
+        pr7 = Number.parseInt(pr7)
+        if(pr7 == 100){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
+}
+function otvet8(){
+    let pr8 = document.getElementById("pr8").value
+        pr8 = Number.parseInt(pr8)
+        if(pr8 == 35){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
+}
+function otvet9(){
+    let pr9 = document.getElementById("pr9").value
+        pr9 = Number.parseInt(pr9)
+        if(pr9 == 297){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
+}
+function otvet10(){
+    let pr10 = document.getElementById("pr10").value
+        pr10 = Number.parseInt(pr10)
+        if(pr10 == 30){
+            alert("Правильный ответ");
+        }
+        else{
+            alert("Неправильный ответ");
+        }
+}
+function kon(){
+    alert("Молодцы, что прошли тест ваше время:");
+}
